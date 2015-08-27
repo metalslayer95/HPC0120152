cee6fe55d5f046fcc06caad0c37ed70074828114
Richard Salazar <rass12398@hotmail.com>
2015-08-26 20:40:13 -0500

    Correccion archivos

---
+++ addVec/sumasecuencial.c
@@ -0,0 +1,57 @@
+
+# define TAM 20000
+#include "malloc.h"
+#include "time.h"
+#include "stdio.h"
+#include "stdlib.h"
+void vecAdd(int *vec1,int *vec2,int *vec3)
+{
+  int i;
+  for ( i = 0 ; i < TAM ; i++)
+  {
+   vec3[i] = vec1[i] + vec2[i];   
+  }  
+}
+
+void initialize(int *vec1,int *vec2)
+{
+  int i;
+ 	srand(time(NULL));
+  for ( i = 0; i< TAM; i++)
+  {
+  	vec1[i] = rand() % (1+5-0) + 0;
+  	vec2[i] = rand() % (1+5-0) + 0;
+  }
+}
+
+void printAdd(int *a,int *b,int *c)
+{
+   int i;
+  for ( i = 0; i < TAM ; i++)
+  { 
+  	printf("%d + %d = %d\n",a[i],b[i],c[i]);
+  } 
+}
+
+main () 
+{
+  int *a,*b,*c;
+  clock_t begin, end;
+	double time_spent;
+	a = NULL;
+  b = NULL;
+  c = NULL;
+  a = (int *) malloc ( sizeof(int) * TAM);
+  b = (int *) malloc ( sizeof(int) * TAM);
+  c = (int *) malloc ( sizeof(int) * TAM);
+  initialize(a,b);
+  begin = clock();
+  vecAdd(a,b,c);
+  end = clock();
+  printAdd(a,b,c);
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("Se ha demorado %f segundos.\n",time_spent);
+  free(a);
+  free(b);
+  free(c);
+}
