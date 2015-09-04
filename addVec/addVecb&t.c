5f48975868774ea7a4adaf693f15398e46d928c9
Richard Salazar <rass12398@hotmail.com>
2015-09-03 21:14:32 -0500

    Clase miercoles 2 sept

--- addVec/addVecb&t.c
+++ addVec/addVecb&t.c
@@ -1,7 +1,8 @@
 		 #include <stdio.h>
 
 		#define HANDLE_ERROR( err ) ( HandleError( err, __FILE__, __LINE__ ) )
-
+		# define num_block 1024
+		# define N 500000
 		static void HandleError( cudaError_t err, const char *file, int line )
 		{
 			if (err != cudaSuccess)
@@ -13,15 +14,12 @@
 		}
 
 
-
-		const short N = 1024 ;
-
 		// CUDA Kernel for Vector Addition
 		__global__ void Vector_Addition ( const float *dev_a , const float *dev_b , float *dev_c)
 		{
 			  //Get the id of thread within a block
 			  //unsigned short tid = threadIdx.x ;
-			  unsigned short tid = threadIdx.x ;
+			  unsigned short tid = blockIdx.x*blockDim.x+threadIdx.x ;
 			 
 			  if ( tid < N ) // check the boundry condition for the threads
 					dev_c [tid] = dev_a[tid] + dev_b[tid] ;
@@ -52,6 +50,7 @@
 					Host_b[i] = i*i ; 
 			  }
 			  
+			  int dimGrid = (int)ceil((float)N/(float)num_block);
 				begin = clock();
 			  //Copy Host array to Device array
 			  HANDLE_ERROR (cudaMemcpy (dev_a , Host_a , N*sizeof(float) , cudaMemcpyHostToDevice));
@@ -59,7 +58,7 @@
 
 
 			  //Make a call to GPU kernel
-			  Vector_Addition <<< 1, N  >>> (dev_a , dev_b , dev_c ) ;
+			  Vector_Addition <<< dimGrid, num_block  >>> (dev_a , dev_b , dev_c ) ;
 
 
 			  //Copy back to Host array from Device array
@@ -67,8 +66,8 @@
 
 		  end = clock();
 			  //Display the result
-			  for ( int i = 0; i<N; i++ )
-						  printf ("%f + %f = %f\n", Host_a[i] , Host_b[i] , Host_c[i] ) ;
+			  //for ( int i = 0; i<N; i++ )
+				//		  printf ("%f + %f = %f\n", Host_a[i] , Host_b[i] , Host_c[i] ) ;
 
 			  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
 			  printf("Se ha demorado %f segundos.\n",time_spent);
