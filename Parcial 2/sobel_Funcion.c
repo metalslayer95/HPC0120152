7aec7dee92c019ee0f8f6ed8f7fe52d6ade42847
utp <utp@localhost.localdomain>
2015-10-27 09:45:33 -0500

    Agregando version secuencial con llamado a sobel opencv

--- Parcial 2/sobel_Funcion.c
+++ Parcial 2/sobel_Funcion.c
@@ -2,6 +2,7 @@
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
+#include "time.h"
 
 using namespace cv;
 
@@ -9,31 +10,31 @@ using namespace cv;
 int main( )
 {
 
+  clock_t begin, end;
   Mat src, src_gray;
   Mat grad;
+ 	float time_spent;
   int scale = 1;
   int delta = 0;
   int ddepth = CV_16S;
 
   int c;
-
+  /// Generate grad_x and grad_y
+  Mat grad_x, grad_y;
+  Mat abs_grad_x, abs_grad_y;
   /// Load an image
   src = imread( "./inputs/img1.jpg" );
 
   if( !src.data )
   { return -1; }
 
+  begin = clock();
   GaussianBlur( src, src, Size(3,3), 0, 0, BORDER_DEFAULT );
 
   /// Convert it to gray
   cvtColor( src, src_gray, CV_RGB2GRAY );
 
-  /// Create window
 
-
-  /// Generate grad_x and grad_y
-  Mat grad_x, grad_y;
-  Mat abs_grad_x, abs_grad_y;
 
   /// Gradient X
   //Scharr( src_gray, grad_x, ddepth, 1, 0, scale, delta, BORDER_DEFAULT );
@@ -45,9 +46,12 @@ int main( )
   Sobel( src_gray, grad_y, ddepth, 0, 1, 3, scale, delta, BORDER_DEFAULT );
   convertScaleAbs( grad_y, abs_grad_y );
 
+
   /// Total Gradient (approximate)
   addWeighted( abs_grad_x, 0.5, abs_grad_y, 0.5, 0, grad );
-
+  end = clock();
+  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+  printf("%f\n",time_spent);
    imwrite("./outputs/1088328019.png",grad);
 
   waitKey(0);
