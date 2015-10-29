ff069f19c9f9fe806c7ead6e9b7b5bc9c06d2bcc
utp <utp@localhost.localdomain>
2015-10-29 12:51:00 -0500

    Agregando algunas cosas

--- Parcial 2/sobel_Parallel.c
+++ Parcial 2/sobel_Parallel.c
@@ -17,54 +17,171 @@ static void HandleError( cudaError_t err, const char *file, int line )
     exit( EXIT_FAILURE );
   }
 }
+#define Mask_size 3
+#define BLOCKSIZE 32
+#define TILE_SIZE 32
 
-__global__ void sobel_Filter(){
+using namespace std;
 
+using namespace cv;
+
+__constant__ char Global_Mask[Mask_size*Mask_size];
+__device__ unsigned char clamp(int value){
+	if(value < 0)
+		value = 0;
+	else
+		if(value > 255)
+			value = 255;
+	return value;
 }
+__global__ void sobelFilter(unsigned char *In, int Row, int Col, unsigned int Mask_Width,char *Mask,unsigned char *Out){
+	unsigned int row = blockIdx.y*blockDim.y+threadIdx.y;
+	unsigned int col = blockIdx.x*blockDim.x+threadIdx.x;
+	int Pvalue = 0;
+	int N_start_point_row = row - (Mask_Width/2);
+	int N_start_point_col = col - (Mask_Width/2);
+	for(int i = 0; i < Mask_Width; i++){
+		for(int j = 0; j < Mask_Width; j++ ){
+			if((N_start_point_col + j >=0 && N_start_point_col + j < Row)&&(N_start_point_row + i >=0 && N_start_point_row + i < Col)){
+			Pvalue += In[(N_start_point_row + i)*Row+(N_start_point_col + j)] * Mask[i*Mask_Width+j];
+			}
+		}
+	}
+	Out[row*Row+col] = clamp(Pvalue);
+}
+__global__ void sobelFilterConstant(unsigned char *In, int Row, int Col, unsigned int Mask_Width,unsigned char *Out){
+	unsigned int row = blockIdx.y*blockDim.y+threadIdx.y;
+	unsigned int col = blockIdx.x*blockDim.x+threadIdx.x;
+	int Pvalue = 0;
+	int N_start_point_row = row - (Mask_Width/2);
+	int N_start_point_col = col - (Mask_Width/2);
+	for(int i = 0; i < Mask_Width; i++){
+		for(int j = 0; j < Mask_Width; j++ ){
+			if((N_start_point_col + j >=0 && N_start_point_col + j < Row)&&(N_start_point_row + i >=0 && N_start_point_row + i < Col)){
+				Pvalue += In[(N_start_point_row + i)*Row+(N_start_point_col + j)] * Global_Mask[i*Mask_Width+j];
+			}
+		}
+	}
+	Out[row*Row+col] = clamp(Pvalue);
+}
+__global__ void sobel_Compartida(unsigned char *In, unsigned char *Out,int maskWidth, int width, int height){
+	__shared__ float N_ds[TILE_SIZE + Mask_size - 1][TILE_SIZE+ Mask_size - 1];
+	int n = Mask_size/2;
+	int dest = threadIdx.y*TILE_SIZE+threadIdx.x, destY = dest / (TILE_SIZE+Mask_size-1), destX = dest % (TILE_SIZE+Mask_size-1),
+	srcY = blockIdx.y * TILE_SIZE + destY - n, srcX = blockIdx.x * TILE_SIZE + destX - n,
+	src = (srcY * width + srcX);
+	if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+	N_ds[destY][destX] = In[src];
+	else
+	N_ds[destY][destX] = 0;
+	// Second batch loading
+	dest = threadIdx.y * TILE_SIZE + threadIdx.x + TILE_SIZE * TILE_SIZE;
+	destY = dest /(TILE_SIZE + Mask_size - 1), destX = dest % (TILE_SIZE + Mask_size - 1);
+	srcY = blockIdx.y * TILE_SIZE + destY - n;
+	srcX = blockIdx.x * TILE_SIZE + destX - n;
+	src = (srcY * width + srcX);
+	if (destY < TILE_SIZE + Mask_size - 1) {
+		if (srcY >= 0 && srcY < height && srcX >= 0 && srcX < width)
+			N_ds[destY][destX] = In[src];
+		else
+			N_ds[destY][destX] = 0;
+		}
+	__syncthreads();
+	int accum = 0;
+	int y, x;
+	for (y = 0; y < maskWidth; y++)
+		for (x = 0; x < maskWidth; x++)
+			accum += N_ds[threadIdx.y + y][threadIdx.x + x] * Global_Mask[y * maskWidth + x];
+		y = blockIdx.y * TILE_SIZE + threadIdx.y;
+		x = blockIdx.x * TILE_SIZE + threadIdx.x;
+		if (y < height && x < width)
+			Out[(y * width + x)] = clamp(accum);
+		__syncthreads();
+	}
+	__global__ void gray(unsigned char *In, unsigned char *Out,int Row, int Col){
+		int row = blockIdx.y*blockDim.y+threadIdx.y;
+		int col = blockIdx.x*blockDim.x+threadIdx.x;
+		if((row < Col) && (col < Row)){
+			Out[row*Row+col] = In[(row*Row+col)*3+2]*0.299 + In[(row*Row+col)*3+1]*0.587+ In[(row*Row+col)*3]*0.114;
+		}
+		}
+		// :::::::::::::::::::::::::::::::::::Clock Function::::::::::::::::::::::::::::
+		double diffclock(clock_t clock1,clock_t clock2){
+		double diffticks=clock2-clock1;
+		double diffms=(diffticks)/(CLOCKS_PER_SEC/1); // /1000 mili
+		return diffms;
+	}
+
+void d_convolution2d(Mat image,unsigned char *In,unsigned char *h_Out,char *h_Mask,int Mask_Width,int Row,int Col,int op){
+	// Variables
+	int size_of_rgb = sizeof(unsigned char)*Row*Col*image.channels();
+	int size_of_Gray = sizeof(unsigned char)*Row*Col; // sin canales alternativos
+	int Mask_size_of_bytes = sizeof(char)*(Mask_size*Mask_size);
+	unsigned char *d_In,*d_Out,*d_sobelOut;
+	char *d_Mask;
+	float Blocksize=BLOCKSIZE;
+	// Memory Allocation in device
+	cudaMalloc((void**)&d_In,size_of_rgb);
+	cudaMalloc((void**)&d_Out,size_of_Gray);
+	cudaMalloc((void**)&d_Mask,Mask_size_of_bytes);
+	cudaMalloc((void**)&d_sobelOut,size_of_Gray);
+	// Memcpy Host to device
+	cudaMemcpy(d_In,In,size_of_rgb, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_Mask,h_Mask,Mask_size_of_bytes,cudaMemcpyHostToDevice);
+	cudaMemcpyToSymbol(Global_Mask,h_Mask,Mask_size_of_bytes); // avoid cache coherence
+	// Thread logic and Kernel call
+	dim3 dimGrid(ceil(Row/Blocksize),ceil(Col/Blocksize),1);
+	dim3 dimBlock(Blocksize,Blocksize,1);
+	gray<<<dimGrid,dimBlock>>>(d_In,d_Out,Row,Col); // pasando a escala de grices.
+	cudaDeviceSynchronize();
+	if(op==1){
+	sobelFilter<<<dimGrid,dimBlock>>>(d_Out,Row,Col,Mask_size,d_Mask,d_sobelOut);
+	}
+	if(op==2){
+	sobelFilterConstant<<<dimGrid,dimBlock>>>(d_Out,Row,Col,Mask_size,d_sobelOut);
+	}
+	if(op==3){
+	sobelFilterShared<<<dimGrid,dimBlock>>>(d_Out,d_sobelOut,3,Row,Col);
+	}
+	cudaMemcpy (h_Out,d_sobelOut,size_of_Gray,cudaMemcpyDeviceToHost);
+	cudaFree(d_In);
+	cudaFree(d_Out);
+	cudaFree(d_Mask);
+	cudaFree(d_sobelOut);
+}
+
 
 
 main () 
 {
-  int *Host_a,*Host_b,*Host_c;
-  int *dev_a,*dev_b,*dev_c;
-  clock_t begin, end;
-  double time_spent;
-  Mat src, src_gray;
-  Mat grad;
-  src = imread( "./inputs/img1.jpg" );
-  Host_a = NULL;
-  Host_b = NULL;
-  Host_c = NULL;
-  Host_a = (int *) malloc ( sizeof(int) * Ar*Ac);
-  Host_b = (int *) malloc ( sizeof(int) * Br*Bc);
-  Host_c = (int *) malloc ( sizeof(int) * Ar*Bc);
-  initialize(Host_a,Ar,Ac);
-  initialize(Host_b,Br,Bc);
-  //Allocate the memory on the GPU
-  HANDLE_ERROR ( cudaMalloc((void **)&dev_a , Ar*Ac*sizeof(int) ) );
-  HANDLE_ERROR ( cudaMalloc((void **)&dev_b , Br*Bc*sizeof(int) ) );
-  HANDLE_ERROR ( cudaMalloc((void **)&dev_c , Ar*Bc*sizeof(int) ) );
-  dim3 dimBlock(32, 32,1);
-  dim3 dimGrid((int)ceil((float)Ar/(float)dimBlock.x),(int)ceil((float)Bc/(float)dimBlock.y),1);
-  begin = clock();
- //Copy Host array to Device array
-  HANDLE_ERROR (cudaMemcpy (dev_a , Host_a , Ar*Ac*sizeof(int) , cudaMemcpyHostToDevice));
-  HANDLE_ERROR (cudaMemcpy (dev_b , Host_b , Br*Bc*sizeof(int) , cudaMemcpyHostToDevice));
-
-
-  //Make a call to GPU kernel
-  matMulti <<< dimGrid, dimBlock  >>> (dev_a , dev_b , dev_c ) ;
-
-
-  //Copy back to Host array from Device array
-  HANDLE_ERROR (cudaMemcpy(Host_c , dev_c , Ar*Bc*sizeof(int) , cudaMemcpyDeviceToHost));
-
-  end = clock();
-
-  imwrite("./outputs/1088328019.png",grad);
-  time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
-  printf("%f\n",time_spent);
-  free(Host_a);
-  free(Host_b);
-  free(Host_c);
+	 double T1,T2; // Time flags
+	clock_t start,end;// Time flags
+	int Mask_Width = Mask_size;
+	char h_Mask[] = {-1,0,1,-2,0,2,-1,0,1};
+	Mat image,result_image;
+	image = imread("./inputs/img1.jpg");
+	Size s = image.size();
+	int Row = s.width;
+	int Col = s.height;
+	unsigned char * In = (unsigned char*)malloc( sizeof(unsigned char)*Row*Col*image.channels());
+	unsigned char * h_Out = (unsigned char *)malloc( sizeof(unsigned char)*Row*Col);
+	In = image.data;
+	start = clock();
+	d_convolution2d(image,In,h_Out,h_Mask,Mask_Width,Row,Col,3);
+	end = clock();
+	T1=diffclock(start,end);
+	cout<<" Result Parallel"<<" At "<<T1<<",Seconds"<<endl;
+	Mat gray_image_opencv, grad_x, abs_grad_x;
+	start = clock();
+	cvtColor(image, gray_image_opencv, CV_BGR2GRAY);
+	Sobel(gray_image_opencv,grad_x,CV_8UC1,1,0,3,1,0,BORDER_DEFAULT);
+	convertScaleAbs(grad_x, abs_grad_x);
+	end = clock();
+	T2=diffclock(start,end);
+	cout<<" Result secuential"<<" At "<<T2<<",Seconds"<<endl;
+	cout<<"Total acceleration "<<T2/T1<<"X"<<endl;
+	result_image.create(Col,Row,CV_8UC1);
+	result_image.data = h_Out;
+	imwrite("./outputs/1088328019.png",result_image);
+	return 0;
 }
---
+++ convolucion/convolusion.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+#include <cuda.h>
+
+#define N_elements 32
+#define Mask_size  5
+#define TILE_SIZE  1024
+#define BLOCK_SIZE 1024
+
+using namespace std;
+
+__constant__ int Global_Mask[Mask_size];
+
+//:::::::::::::::::::::::::::: Device Kernel Function ::::::::::::::::::::::::::::::
+
+__global__ void convolution1d_tiles_constant_kernel(int *In, int *Out){
+  unsigned int index = blockIdx.x * blockDim.x + threadIdx.x; // Index 1d iterator.
+  __shared__ int Tile[TILE_SIZE + Mask_size - 1];
+  int n = Mask_size/2;
+  int halo_left_index  = (blockIdx.x - 1 ) * blockDim.x + threadIdx.x;
+  if (threadIdx.x  >= blockDim.x - n ){
+     Tile[threadIdx.x - (blockDim.x - n )] = (halo_left_index < 0) ? 0 : In[halo_left_index];
+  }
+
+  if(index<N_elements){Tile[n + threadIdx.x] = In[index];
+  }else{Tile[n + threadIdx.x] = 0;}
+  int halo_right_index = (blockIdx.x + 1 ) * blockDim.x + threadIdx.x;
+  if (threadIdx.x < n) {
+    Tile[n + blockDim.x + threadIdx.x]=  (halo_right_index >= N_elements) ? 0 : In[halo_right_index];
+  }
+
+__syncthreads();
+  int Value = 0;
+  for (unsigned int j = 0; j  < Mask_size; j ++) {
+    Value += Tile[threadIdx.x + j] * Global_Mask[j];
+  }
+  Out[index] = Value;
+}
+
+__global__ void convolution1d_notile_noconstant_kernel(int *In, int *Out){
+  unsigned int index = blockIdx.x * blockDim.x + threadIdx.x; // Index 1d iterator.
+  int Value = 0;
+  int N_start_point = index - (Mask_size/2);
+  for ( int j = 0; j  < Mask_size; j ++) {
+    if (N_start_point + j >= 0 && N_start_point + j < N_elements) {
+      Value += In[N_start_point + j] * Global_Mask[j];
+    }
+  }
+  Out[index] = Value;
+}
+
+__global__ void convolution1d_constant_simple_kernel(int *In, int *Out){
+
+  int i = blockIdx.x*blockDim.x + threadIdx.x;
+  __shared__ float N_ds[TILE_SIZE];
+  N_ds[threadIdx.x] = In[i];
+  __syncthreads();
+  int This_tile_start_point = blockIdx.x * blockDim.x;
+  int Next_tile_start_point = (blockIdx.x + 1) * blockDim.x;
+  int N_start_point = i - (Mask_size/2);
+  int Pvalue = 0;
+  for (int j = 0; j < Mask_size; j ++){
+    int N_index = N_start_point + j;
+    if (N_index >= 0 && N_index < N_elements){
+      if ((N_index >= This_tile_start_point) && (N_index < Next_tile_start_point)){
+        Pvalue += N_ds[threadIdx.x+j-(Mask_size/2)]*Global_Mask[j];
+      } else{ Pvalue += In[N_index] * Global_Mask[j]; }
+    }
+  }
+  Out[i] = Pvalue;
+}
+
+//:: Invocation Function
+
+void d_convolution1d(int *In,int *Out,int *h_Mask,int op){
+  // Variables
+  int Size_of_bytes = N_elements * sizeof(int);
+  int *d_In, *d_Out;
+  float Blocksize=BLOCK_SIZE;
+  d_In = (int*)malloc(Size_of_bytes);
+  d_Out = (int*)malloc(Size_of_bytes);
+  // Memory Allocation in device
+  cudaMalloc((void**)&d_In,Size_of_bytes);
+  cudaMalloc((void**)&d_Out,Size_of_bytes);
+  // Memcpy Host to device
+  cudaMemcpy(d_In,In,Size_of_bytes,cudaMemcpyHostToDevice);
+  cudaMemcpy(d_Out,Out,Size_of_bytes,cudaMemcpyHostToDevice);
+  cudaMemcpyToSymbol(Global_Mask,h_Mask,Mask_size*sizeof(int)); // avoid cache coherence
+  // Thead logic and Kernel call
+  dim3 dimGrid(ceil(N_elements/Blocksize),1,1);
+  dim3 dimBlock(Blocksize,1,1);
+  if(op==1){
+    cout<<"convolution1d tile constant "<<endl;
+    convolution1d_tiles_constant_kernel<<<dimGrid,dimBlock>>>(d_In,d_Out);
+  }
+  if(op==2){
+    cout<<"convolution1d notile noconstant "<<endl;
+    convolution1d_notile_noconstant_kernel<<<dimGrid,dimBlock>>>(d_In,d_Out);
+  }
+  if (op==3) {
+    cout<<"convolution1d constant tile simple "<<endl;
+    convolution1d_constant_simple_kernel<<<dimGrid,dimBlock>>>(d_In,d_Out);
+  }
+
+
+  cudaDeviceSynchronize();
+  // save output result.
+  cudaMemcpy (Out,d_Out,Size_of_bytes,cudaMemcpyDeviceToHost);
+  // Free device memory
+  cudaFree(d_In);
+  cudaFree(d_Out);
+  //cudaFree(d_Mask);
+}
+
+//:::::::::::::::::::::::::::: Host Function ::::::::::::::::::::::::::::::
+
+void h_Convolution_1d(int *In,int *Out, int *Mask){
+  for(int i=0;i<N_elements;i++){
+    int Gap=i-(Mask_size)/2; // asymmetric Gap (Left Right)
+    int Value=0;
+    for(int j=0;j<Mask_size;j++){
+      if(Gap+j >= 0 && j+Gap<N_elements){
+        Value+=In[Gap+j]*Mask[j];
+      }// end if
+    }// end for j
+    Out[i]=Value;
+  }// end for i
+}
+//:::::::::::::::::::::::::::: Rutinary Functions ::::::::::::::::::::::::::::::
+
+void Fill_elements(int * VecIn1,int Value, int n){
+    for (int i = 0; i < n; i++) {
+          VecIn1[i]=Value;
+    }
+}
+
+void Show_vec(int *Vec,int Elements,char * Msg ){
+  cout<<Msg<<endl;
+  for (int i=0;i<Elements;i++){
+    if(i%10==0 && i!=0){
+      cout<<endl;
+    }
+    cout<<"["<<Vec[i]<<"] ";
+  }
+  cout<<endl;
+}
+
+
+// :::::::::::::::::::::::::::::::::::Clock Function::::::::::::::::::::::::::::
+double diffclock(clock_t clock1,clock_t clock2){
+  double diffticks=clock2-clock1;
+  double diffms=(diffticks)/(CLOCKS_PER_SEC/1); // /1000 mili
+  return diffms;
+}
+// :::::::::::::::::::::::::::::::::::::::Main::::::::::::::::::::::::::::::::::
+
+int main(){
+
+  double T1,T2; // Time flags
+  clock_t start,end;// Time flags
+
+  int *VecIn1=(int*)malloc(N_elements*sizeof(int)); // Sequential and Parallel Vector Input
+  int *VecOut1=(int*)malloc(N_elements*sizeof(int)); // Sequential Vector Output
+  int *VecOut2=(int*)malloc(N_elements*sizeof(int)); // Parallel Vector Output
+  int *Mask=(int*)malloc(Mask_size*sizeof(int)); // Mask Vector;
+
+  Fill_elements(VecIn1,1,N_elements);
+  Fill_elements(Mask,1,Mask_size);
+
+  start = clock();
+	h_Convolution_1d(VecIn1,VecOut1,Mask);
+  end = clock();
+  T1=diffclock(start,end);
+  cout<<"SEcuencial"<<" a "<<T1<<",segundos"<<endl;
+
+  start = clock();
+  d_convolution1d(VecIn1,VecOut2,Mask,3); // 1 --> List of kernels
+  end = clock();
+  T2=diffclock(start,end);
+  cout<<"Resultado en paralelo"<<" es "<<T2<<",segundos"<<endl;
+  //Show_vec(VecOut2,N_elements,(char *)"Vector Out");
+  cout<<"Aceleracion total "<<T1/T2<<" Veces "<<endl;
+
+  // Releasing Memory
+
+  free(VecIn1);
+  free(VecOut1);
+  free(VecOut2);
+  free(Mask);
+
+  return 0;
+}
