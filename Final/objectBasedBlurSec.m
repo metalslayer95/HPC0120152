9ae8c2fcdcaf4f47ea0e86143d0309005ebb8cfb
metalslayer95 <rass12398@hotmail.com>
2015-12-01 09:02:52 -0500

    Agregando codigos funcionales falta organizar

--- Final/objectBasedBlurSec.m
+++ Final/objectBasedBlurSec.m
@@ -1,39 +1,19 @@
-I = imread('imgPrueba10.jpg');
+I = imread('imgPrueba6.jpg');
+auxI = I;
 nFilters = 3;
-%%%
+[n,m,ch] = size(auxI);
 Ibw = rgb2gray(I);
-ind = find(Ibw < 135);
-ind2 = find(Ibw >= 135);
+ind = find(Ibw < 165);
+ind2 = find(Ibw >= 165);
 Ibw(ind) = 0;
 Ibw(ind2) = 255;
 b = bwperim(Ibw,8); 
-%[b, num]=CapBinaria(Ibw);         %Obtener imagen en FORMATO BINARIO
 [B,L]= bwboundaries(b,'holes');  %Agujeros negros
 figure(1)
-fill=imfill(L,'holes');          %Lenar agujeros
+fill=imfill(L,'holes');          %Llenar agujeros
 Ibw = imfill(fill,'holes');
-imshow(fill);
-% [Ilabel, Ne]= bwlabel(Ibw);          %Ne numero de objjtos blancos
-% stat = regionprops(Ilabel, 'centroid');     %Obtener(Area,Centroide,Limites) 
-% imshow(I) 
-hold on
-% for x = 1: numel(stat)
-%     if numel(stat(x))<2  
-%         plot(stat(x).Centroid(1),stat(x).Centroid(2),'x');   
-%         xc=stat(x).Centroid(1);
-%         yc=stat(x).Centroid(2);
-%         radius=25;
-%         theta = 0:0.01:2*pi;
-%         Xfit = radius*cos(theta) + xc;
-%         Yfit = radius*sin(theta) + yc;
-%         plot(Xfit, Yfit, 'y', 'LineWidth', 4);
-%         fprintf('\n--------OBJETO DETECTADO--------\n');
-%         fprintf('\nPosicion:\n');
-%         fprintf('Abscisa%10.3f\n',xc)
-%         fprintf('Ordenada%10.3f\n',yc)
-%     end
-% end
-%%%%
+imshow(Ibw);
+
 %%% Filtrado con sobel
 % tic
 % sobelFilter = fspecial('sobel');
@@ -46,18 +26,39 @@ hold on
 tic
 blurFilter = fspecial('disk',5);
 diskBlur = imfilter(I,blurFilter,'replicate');
+for i=1:n 
+     for j =1:m 
+         if (Ibw(i,j) > 0)
+             diskBlur(i,j,:) = auxI(i,j,:);
+         end
+     end
+end
 timedisk = toc
 
 %%% aplicando blur con "motion"
 tic
-motionFilter = fspecial('motion',20,45);
+motionFilter = fspecial('motion',10,270);
 motionBlur = imfilter(I,motionFilter,'replicate','conv');
+for i=1:n 
+     for j =1:m 
+         if (Ibw(i,j) > 0)
+             motionBlur(i,j,:) = auxI(i,j,:);
+         end
+     end
+end
 timemotion = toc
 
 %%% aplicando blur con "gaussian"
 tic
 gaussianFilter = fspecial('gaussian',5,10);
 gaussianBlur = imfilter(I,gaussianFilter,'replicate','conv');
+for i=1:n 
+     for j =1:m 
+         if (Ibw(i,j) > 0)
+             gaussianBlur(i,j,:) = auxI(i,j,:);
+         end
+     end
+end
 timegaussian = toc
 
 figure(2)
@@ -65,32 +66,18 @@ figure(2)
 imshow(I)
 title('Original');
 figure(3)
-%%% graficando imagen con disk
-subplot(1,nFilters,1)
+% %%% graficando imagen con disk
+% %subplot(1,nFilters,1)
 imshow(diskBlur)
 title('Disk')
 
-%%% graficando imagen con motion
-subplot(1,nFilters,2)
-imshow(motionBlur)
-title('Motion')
-
+% %%% graficando imagen con motion
+% subplot(1,nFilters,2)
+% imshow(motionBlur)
+% title('Motion')
+% 
 %%% graficando imagen con gaussian
-subplot(1,nFilters,3)
-imshow(gaussianBlur)
-title('Gaussian')
-
-%%%%
-% figure(3)
-% Ibw = rgb2gray(I);
-% ind = find(Ibw < 185);
-% ind2 = find(Ibw >= 185);
-% Ibw(ind) = 0;
-% Ibw(ind2) = 255;
-% I2 = bwperim(Ibw,8);
-% imshow(I2);
-%hold on
-%plot(I2);
-
-
+% subplot(1,nFilters,3)
+% imshow(gaussianBlur)
+% title('Gaussian')
 
